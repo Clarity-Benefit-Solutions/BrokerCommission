25daeb44db027d4a8384d353b5b9c2fc678404a7
Sumeet Kumar <sumeet@toptalc.om>
2022-05-12 03:40:02 -0400

    update edmx model and namespace

--- src/Apps/BrokerCommissionWebApp/DataModel/Broker_CommissionEntities.Designer.cs
+++ src/Apps/BrokerCommissionWebApp/DataModel/Broker_CommissionEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\BROKERCOMMISSION\Broker_Commission\src\Apps\BrokerCommissionWebApp\DataModel\system_model.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\BROKERCOMMISSION\Broker_Commission\src\Apps\BrokerCommissionWebApp\DataModel\Broker_CommissionEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
