10f272e7de3239d5376749e9e7d822c6258ce105
Sumeet Kumar <sumeet@toptalc.om>
2022-05-26 11:45:58 -0400

    update dbmodel from db

---
+++ _SQLFiles/.idea/dataSources/b72e394e-8e1a-4ad1-8d55-9b2e32274a19/storage_v2/_staging_/database/Broker_Commission.EUXFFw/schema/dbo.sYMBAA/view/DASH_BOARD.2cuHvQ.sql
@@ -0,0 +1,17 @@
+ALTER VIEW [dbo].[DASH_BOARD]
+    AS
+        (
+            
+            SELECT
+                BM.*
+              , dbo.get_broker_commission_paid_amount( ID , dbo.get_current_statement_month( ) ,
+                                                       dbo.get_current_statement_year( ) ) +
+                dbo.get_broker_commission_pending_amount( ID , dbo.get_current_statement_month( ) ,
+                                                          dbo.get_current_statement_year( ) )
+                    TOTAL_AMOUNT
+            
+            FROM
+                [dbo].[BROKER_MASTER] AS BM
+        )
+go
+
